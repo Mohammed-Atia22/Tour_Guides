0315a3fa37c91cffbd78d92897c1ac9d2b638ced
Mohamed_Atia <muhamedatia22@gmail.com>
2025-05-15 06:58:23 +0300

    perform some changes in connect file

--- db/connect.js
+++ db/connect.js
@@ -6,10 +6,11 @@ const util = require('util');
 
 
 const pool = mysql.createPool({
-    host:process.env.MYSQL_HOST,
-    user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DATABASE,
+    host:process.env.MYSQLHOST,
+    port:process.env.MYSQLPORT,
+    user:process.env.MYSQLUSER,
+    password:process.env.MYSQLPASSWORD,
+    database:process.env.MYSQLDATABASE,
     waitForConnections:true,
     connectionLimit: 10,
     queueLimit: 0
